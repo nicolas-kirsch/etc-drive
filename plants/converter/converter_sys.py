22a43f2543022fc39e053bb90c9afde3c4aa30e4
Nicolas Kirsch <nicolaskirsch@Nicolass-MacBook-Pro-5.local>
2025-09-18 14:50:33 +0200

    Debug device

--- plants/converter/converter_sys.py
+++ plants/converter/converter_sys.py
@@ -362,10 +362,12 @@ class Converter(torch.nn.Module):
 
     def gen_mg_G2(self,ms: torch.Tensor):
 
-        ms_norm = torch.linalg.norm(ms, ord=2, dim=-1, keepdim=True)  # Shape: (batch_size, 1, 1)
+        print("ms",ms.device)
 
+        ms_norm = torch.linalg.norm(ms, ord=2, dim=-1, keepdim=True)  # Shape: (batch_size, 1, 1)
+        print("ms_norm",ms_norm.device)
         ms_lin_norm_sat = torch.minimum(ms_norm, torch.full_like(ms_norm, self.mg_max).to(device))
-        ms_norm_sat = 1/25*torch.log(1/(torch.exp(-25*ms_norm)+torch.exp(-25*torch.tensor([self.mg_max*0.99]))))  # Smooth saturation function
+        ms_norm_sat = 1/25*torch.log(1/(torch.exp(-25*ms_norm)+torch.exp(-25*torch.tensor([self.mg_max*0.99]).to(device))))  # Smooth saturation function
 
         Gamma_2 = ms_norm - ms_norm_sat
 
